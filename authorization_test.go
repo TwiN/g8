7e263fcc25a60e21c29eb602e86fc9cfedfac6a7
TwinProduction <twin@twinnation.org>
2021-01-03 17:25:46 -0500

    Improve test coverage

--- authorization_test.go
+++ authorization_test.go
@@ -8,7 +8,13 @@ func TestAuthorizationService_IsAuthorized(t *testing.T) {
 		t.Error("should've returned true")
 	}
 	if authorizationService.IsAuthorized("bad-token", nil) {
-		t.Error("should've returned true")
+		t.Error("should've returned false")
+	}
+	if authorizationService.IsAuthorized("token", []string{"admin"}) {
+		t.Error("should've returned false")
+	}
+	if authorizationService.IsAuthorized("", nil) {
+		t.Error("should've returned false")
 	}
 }
 
@@ -36,6 +42,67 @@ func TestAuthorizationService_IsAuthorizedWithPermissions(t *testing.T) {
 		t.Error("should've returned false")
 	}
 	if authorizationService.IsAuthorized("bad-token", []string{"a"}) {
+		t.Error("should've returned false")
+	}
+	if authorizationService.IsAuthorized("", []string{"a"}) {
+		t.Error("should've returned false")
+	}
+}
+
+func TestAuthorizationService_WithToken(t *testing.T) {
+	authorizationService := NewAuthorizationService().WithToken("token")
+	if !authorizationService.IsAuthorized("token", nil) {
+		t.Error("should've returned true")
+	}
+	if authorizationService.IsAuthorized("bad-token", nil) {
+		t.Error("should've returned false")
+	}
+	if authorizationService.IsAuthorized("token", []string{"admin"}) {
+		t.Error("should've returned false")
+	}
+}
+
+func TestAuthorizationService_WithTokens(t *testing.T) {
+	authorizationService := NewAuthorizationService().WithTokens([]string{"1", "2"})
+	if !authorizationService.IsAuthorized("1", nil) {
+		t.Error("should've returned true")
+	}
+	if !authorizationService.IsAuthorized("2", nil) {
+		t.Error("should've returned true")
+	}
+	if authorizationService.IsAuthorized("3", nil) {
+		t.Error("should've returned false")
+	}
+}
+
+func TestAuthorizationService_WithClient(t *testing.T) {
+	authorizationService := NewAuthorizationService().WithClient(NewClient("token").WithPermissions([]string{"a", "b"}))
+	if !authorizationService.IsAuthorized("token", []string{"a", "b"}) {
+		t.Error("should've returned true")
+	}
+	if !authorizationService.IsAuthorized("token", []string{"a"}) {
+		t.Error("should've returned true")
+	}
+	if !authorizationService.IsAuthorized("token", []string{"b"}) {
+		t.Error("should've returned true")
+	}
+	if authorizationService.IsAuthorized("token", []string{"c"}) {
+		t.Error("should've returned false")
+	}
+}
+
+func TestAuthorizationService_WithClients(t *testing.T) {
+	authorizationService := NewAuthorizationService().WithClients([]*Client{NewClient("1").WithPermission("a"), NewClient("2").WithPermission("b")})
+	if !authorizationService.IsAuthorized("1", []string{"a"}) {
+		t.Error("should've returned true")
+	}
+	if !authorizationService.IsAuthorized("2", []string{"b"}) {
+		t.Error("should've returned true")
+	}
+	if authorizationService.IsAuthorized("1", []string{"b"}) {
+		t.Error("should've returned false")
+	}
+	if authorizationService.IsAuthorized("2", []string{"a"}) {
 		t.Error("should've returned false")
 	}
 }
